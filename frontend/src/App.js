66cc90c4fc225cbd0d693c580a363d6d776b56be
DerpyTheSpy <erickim334@gmail.com>
2022-12-04 10:53:56 -0500

    i tried.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import {BrowserRouter as Router , Route} from 'react-router-dom'
+import Create from './components/create'
+import Read from './components/read'
+import Update from './components/update'
+import Login from './components/login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+   <div classname="heading">FORMS
+   <div>
+      <Route exact path='/create' component ={Create} />
+   </div>
+   <div style={{marginTop:20}} >
+      <Route exact path='/read' component ={Read} />
+   </div>
+   <div style={{marginTop:20}} >
+      <Route exact path='/update' component ={Update} />
+   </div>
+   <div style= {{marginTop:20}} >
+      <Route  exact path='/' component ={Login} />
+   </div>
+   </div>
+   </Router>
   );
 }
 
